854779433b45fd024fefe2b5836740f578beca1d
JJwachi23 <wachimoolang23@gmail.com>
2026-01-11 16:24:59 +0700

    Add: Link for final-python test

---
+++ index.py
@@ -0,0 +1,23 @@
+# # Weight / (Height * Height) 
+
+# weight = float(input("Please enter your weight: "))
+# print(weight)
+
+# height = float(input("Please enter your height: "))
+# print(height)
+
+# convert_to_meter = height / 100
+# print(convert_to_meter)
+
+# height_meter = convert_to_meter * convert_to_meter
+# print(height_meter)
+
+# BMI = weight / height_meter
+# print(BMI)
+
+# print(type(BMI))
+
+personal = ['fullName', 'lastName', 13, 'address']
+
+x = len(personal)
+print(x)
