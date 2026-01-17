aef21462da290d6981c28796521c3db335a9ea67
JJwachi23 <wachimoolang23@gmail.com>
2026-01-17 13:11:18 +0700

    Update: Link for test final python101

--- index.py
+++ index.py
@@ -1,23 +1,2 @@
-# # Weight / (Height * Height) 
-
-# weight = float(input("Please enter your weight: "))
-# print(weight)
-
-# height = float(input("Please enter your height: "))
-# print(height)
-
-# convert_to_meter = height / 100
-# print(convert_to_meter)
-
-# height_meter = convert_to_meter * convert_to_meter
-# print(height_meter)
-
-# BMI = weight / height_meter
-# print(BMI)
-
-# print(type(BMI))
-
-personal = ['fullName', 'lastName', 13, 'address']
-
-x = len(personal)
-print(x)
+for i in range(1, 4):
+    print(i)
